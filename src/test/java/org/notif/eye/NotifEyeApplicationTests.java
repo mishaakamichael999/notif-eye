506dc55257496988835023ba662772ca5a6ebd08
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-03-01 19:27:01 +0200

    Initial commit

---
+++ src/test/java/org/notif/eye/NotifEyeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.notif.eye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifEyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
